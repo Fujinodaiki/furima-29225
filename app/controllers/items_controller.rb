059a14f08165dc5718fb6083dd85065233831464
Fujinodaiki <523.snow@gmail.com>
2020-09-11 19:33:26 +0900

    商品の保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,32 @@
 class ItemsController < ApplicationController
+ before_action :move_to_sign_up, except: [:index,]
+
   def index
   end
 
   def new
+    @item =Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+ private 
+  def item_params
+    params.require(:item).permit(:name,:image,:comment,:price,:category_id,:status_id,:deliveryfee_id,:areaofdelivery_id,:daysunilshipping_id).merge(user_id: current_user.id)
+  end
+
+
+  def move_to_sign_up
+    unless user_signed_in?
+      redirect_to "/users/sign_in"
+    end
+  end
 end
