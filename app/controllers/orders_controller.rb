cef8de7960be3dc058b5520e89a241371a55bc2a
Fujinodaiki <523.snow@gmail.com>
2020-09-18 12:29:05 +0900

    Revert "Revert "購入機能とoderとshippingaddreのテーブルに同時に保存する仕組みの実装""
    This reverts commit 5506b4147059b03a10a38c148d3272da823a6faf.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,38 @@
 class OrdersController < ApplicationController
   def index
-    @item = Item.find(params[:format])
+    @itemshipping = ItemShipping.new
+    @item = Item.find(params[:item_id])
   end
 
+  # def new
+  #   @itemshipping = ItemSgipping.new
+  # end
+
   def create
+    @itemshipping = ItemShipping.new(order_params)
+    if @itemshipping.valid?
+      pay_item
+      @itemshipping.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
   
+  private
+
+  def order_params
+    params.require(:item_shipping).permit(:postalcode,:municipalities,:areaofdelivery_id,:address,:buildingname,:phonenumber).merge( user_id: current_user.id,token:params[:token],item_id:params[:item_id])
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
