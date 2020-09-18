2ee915022e18f8873070b36060acdf60fe10e3b5
Fujinodaiki <523.snow@gmail.com>
2020-09-18 15:32:51 +0900

    プルリクエスト修正

--- app/forms/item_shipping.rb
+++ app/forms/item_shipping.rb
@@ -1,21 +1,16 @@
 class ItemShipping
   include ActiveModel::Model
-  attr_accessor :postalcode
-  attr_accessor :municipalities
-  attr_accessor :areaofdelivery_id
-  attr_accessor :address
-  attr_accessor :buildingname
-  attr_accessor :phonenumber
-  attr_accessor :token
-  attr_accessor :user_id
-  attr_accessor :item_id
+  attr_accessor :postalcode,:municipalities,:areaofdelivery_id,:address,:buildingname,:phonenumber,:token,:user_id,:item_id
+
+  POSTAL_CODE_REGEX = /\A[0-9]{3}-[0-9]{4}\z/
+  PHONNEMUBER_CODE_REGEX = /\A\d{11}\z/
 
   with_options presence: true do
-    validates :postalcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :postalcode, format: { with: POSTAL_CODE_REGEX }
     validates :municipalities
     validates :areaofdelivery_id, numericality: { other_than: 1 }
     validates :address
-    validates :phonenumber, format: { with: /\A\d{11}\z/ }
+    validates :phonenumber, format: { with: PHONNEMUBER_CODE_REGEX }
     validates :user_id
     validates :item_id
     validates :token
