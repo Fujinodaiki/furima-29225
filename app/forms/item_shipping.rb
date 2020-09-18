c744298d612d4b6fd39629ae1be592524410003b
Fujinodaiki <523.snow@gmail.com>
2020-09-18 12:27:18 +0900

    Revert "バリデーションを記述"
    This reverts commit 691a1cafe7b92c93365a068c4febf7041be310eb.

--- app/forms/item_shipping.rb
+++ app/forms/item_shipping.rb
@@ -11,16 +11,7 @@ class ItemShipping
   attr_accessor :user_id
   attr_accessor :item_id
   
-  with_options presence: true do
-    validates :postalcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :municipalities
-    validates :areaofdelivery_id, numericality: { other_than: 1 }
-    validates :address
-    validates :phonenumber, format: { with: /\A\d{11}\z/ }
-    validates :user_id
-    validates :item_id
-    validates :token
-  end
+
   
   def save
     order= Order.create(user_id: user_id, item_id: item_id)
