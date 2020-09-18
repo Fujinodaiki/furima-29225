4335a289504b4e9301c1d996f941f0d77bcddd64
Fujinodaiki <523.snow@gmail.com>
2020-09-18 12:27:43 +0900

    Revert "購入画面のビューファイルを実装"
    This reverts commit b00c24fc6f0226d07b2ac1f14ccc224e651e44da.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,4 @@
 class OrdersController < ApplicationController
   def index
-    @item = Item.find(params[:format])
   end
-
-  def create
-  end
-  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,2 @@
 class Order < ApplicationRecord
-  belongs_to :item
-  has_one    :shippingaddre
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :areaofdelivery
-  validates :areaofdelivery_id, numericality: { other_than: 1 }, presence: true
 end
