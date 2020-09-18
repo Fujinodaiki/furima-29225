c9fdb2c074647bda11cf0159fe4f14c06507f5c7
Fujinodaiki <523.snow@gmail.com>
2020-09-18 12:29:27 +0900

    Revert "Revert "Rubcopの自動修正機能を実行""
    This reverts commit 3ffaf74aaba2708f14ddf756d9ea59ad4384cfe4.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,22 +12,21 @@ class OrdersController < ApplicationController
     if @itemshipping.valid?
       pay_item
       @itemshipping.save
-      return redirect_to root_path
+      redirect_to root_path
     else
       render 'index'
     end
   end
 
-
   private
 
   def order_params
-    params.require(:item_shipping).permit(:postalcode,:municipalities,:areaofdelivery_id,:address,:buildingname,:phonenumber).merge( user_id: current_user.id,token:params[:token],item_id:params[:item_id])
+    params.require(:item_shipping).permit(:postalcode, :municipalities, :areaofdelivery_id, :address, :buildingname, :phonenumber).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
   end
 
   def pay_item
     @item = Item.find(params[:item_id])
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price,
       card: params[:token],
@@ -37,9 +36,6 @@ class OrdersController < ApplicationController
 
   def move_to_root
     @item = Item.find(params[:item_id])
-    if  current_user.id == @item.user_id || @item.order.present? 
-      redirect_to root_path
-    end
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
-
 end
